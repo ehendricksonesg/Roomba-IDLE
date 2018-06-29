459b7b9a408447d07746e94dbcd1a127f2f4e798
ehendricksonesg <ethan.hendrickson@esg-usa.com>
2018-06-29 15:33:43 -0400

    battery-pull add

---
+++ battery-pull.py
@@ -0,0 +1,27 @@
+"""
+This script is used to constantly pull the Roomba's current battery capacity.
+"""
+try:  # Python 3  # create2api library is not compatible in it's current form
+    from tkinter import ttk
+    from tkinter import *  # causes tk widgets to be upgraded by ttk widgets
+    import datetime
+
+except ImportError:  # Python 2
+    import sys, traceback  # trap exceptions
+    import os  # switch off auto key repeat
+    import Tkinter
+    import ttk
+    from Tkinter import *  # causes tk widgets to be upgraded by ttk widgets
+    import tkFont as font  # button font sizing
+    import json  # Create2API JSON file
+    import create2api  # change serial port to '/dev/ttyAMA0'
+    import datetime  # time comparison for Create2 sleep prevention routine
+    import time  # sleep function
+    import threading  # used to timeout Create2 function calls if iRobot has gone to sleep
+    import math  # direction indicator (polygon) rotation
+    import RPi.GPIO as GPIO  # BRC pin pulse
+
+
+while True:
+    print("yes")
+    time.sleep(1)
