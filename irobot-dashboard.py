0b28bf2b94ca25a79a72c14d10e3801c7d1a8208
ehendricksonesg <ethan.hendrickson@esg-usa.com>
2018-07-11 11:12:49 -0400

    dashboard update
    - Removed monitoring for Ubuntu testing

--- irobot-dashboard.py
+++ irobot-dashboard.py
@@ -1155,7 +1155,7 @@ def main():
     
 
 if __name__ == '__main__':
-    auklet_monitoring = Monitoring("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiYWRjYjRiNGQtMDliOC00MWE5LTk5NzAtMGI5YWUwNzA3ZjlhIiwidXNlcm5hbWUiOiI1ODBjMmNkNS00Mzg2LTRkNDYtOGNmMi03NWY3ZjMxYjY1NTAiLCJleHAiOjE1MzA5MDAzNTcsImVtYWlsIjoiIn0.aJ_K4D8fFCprWDajtdA6qJD3KIETnOyBGnNauNT3BY0", "qXa285XCSPnQTk7aYXe5TN", monitoring=True)
-    auklet_monitoring.start()
+    # auklet_monitoring = Monitoring("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiYWRjYjRiNGQtMDliOC00MWE5LTk5NzAtMGI5YWUwNzA3ZjlhIiwidXNlcm5hbWUiOiI1ODBjMmNkNS00Mzg2LTRkNDYtOGNmMi03NWY3ZjMxYjY1NTAiLCJleHAiOjE1MzA5MDAzNTcsImVtYWlsIjoiIn0.aJ_K4D8fFCprWDajtdA6qJD3KIETnOyBGnNauNT3BY0", "qXa285XCSPnQTk7aYXe5TN", monitoring=True)
+    # auklet_monitoring.start()
     main()
-    auklet_monitoring.stop()
+    # auklet_monitoring.stop()
