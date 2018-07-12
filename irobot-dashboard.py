c7d4826f8e1c1ad5dec6bb881a24f5acca35966a
ehendricksonesg <ethan.hendrickson@esg-usa.com>
2018-07-12 16:12:50 -0400

    video update
    - removed try and except parts of import block

--- irobot-dashboard.py
+++ irobot-dashboard.py
@@ -78,27 +78,27 @@ Tkinter reference:
 
 from tkinter import font
 
-try:                        # Python 3  # create2api library is not compatible in it's current form
-    from tkinter import ttk
-    from tkinter import *   # causes tk widgets to be upgraded by ttk widgets
-    import datetime
+# try:                        # Python 3  # create2api library is not compatible in it's current form
+from tkinter import ttk
+from tkinter import *   # causes tk widgets to be upgraded by ttk widgets
+import datetime
     
-except ImportError:         # Python 2
-    import sys, traceback   # trap exceptions
-    import os               # switch off auto key repeat
-    import Tkinter
-    import ttk
-    from Tkinter import *   # causes tk widgets to be upgraded by ttk widgets
-    import tkFont as font   # button font sizing
-    import json             # Create2API JSON file
-    import create2api       # change serial port to '/dev/ttyAMA0'
-    import datetime         # time comparison for Create2 sleep prevention routine
-    import time             # sleep function
-    import threading        # used to timeout Create2 function calls if iRobot has gone to sleep
-    import math             # direction indicator (polygon) rotation
-    import RPi.GPIO as GPIO # BRC pin pulse
-    import csv
-    from auklet.monitoring import Monitoring
+# except ImportError:         # Python 2
+import sys, traceback   # trap exceptions
+import os               # switch off auto key repeat
+import Tkinter
+import ttk
+from Tkinter import *   # causes tk widgets to be upgraded by ttk widgets
+import tkFont as font   # button font sizing
+import json             # Create2API JSON file
+import create2api       # change serial port to '/dev/ttyAMA0'
+import datetime         # time comparison for Create2 sleep prevention routine
+import time             # sleep function
+import threading        # used to timeout Create2 function calls if iRobot has gone to sleep
+import math             # direction indicator (polygon) rotation
+import RPi.GPIO as GPIO # BRC pin pulse
+import csv
+from auklet.monitoring import Monitoring
 
 
 class Dashboard():
